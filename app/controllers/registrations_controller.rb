706f16cf1c18044f27b5b3c04d769423059dd8e1
unquietmind <unquietmind9@gmail.com>
2015-09-27 16:49:26 +0000

    Added header

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
     end
     
     def account_update_params
-        params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :username)
+        params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :username, :age, :bio, :gender)
     end
 end

58f2dd3ea78c06fed0dfd065cd702f2cd3ee3e9f
unquietmind <unquietmind9@gmail.com>
2015-10-09 12:48:45 +0000

    Friendships actions now possible

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#home'
   resources :users, only: [:show, :index]
-  
+  resources :friendships, only: [:create, :destroy, :accept] do
+    member do
+      put :accept
+    end
+  end
 end
